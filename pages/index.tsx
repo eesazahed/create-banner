f5ce4c20283cee1d24d4f569ab701ea260de2f8b
Eesa Zahed <eszhd1@gmail.com>
2023-02-03 17:23:46 -0500

    change default text

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,8 +34,8 @@ const linkStyle = "text-blue-600 hover:text-blue-700 dark:text-blue-500";
 
 const Home: NextPage = () => {
   const [formData, setFormData] = useState<FormData>({
-    title: "My name",
-    subtitle: "Occupation",
+    title: "Title",
+    subtitle: "Subtitle",
     backgroundColor: "darkblue",
     textBackground: "cyan, lime",
     bgColDir: 90,
